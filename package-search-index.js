fc263609749cfa8d0c288e6ba739d7b75a1c2717
jorisguffens <joris.guffens@outlook.com>
2022-02-17 16:50:44 +0000

    Deploying to javadocs from @ MinestomBrick/BrickMovableStructures@52793a7acd7deb212b0c59f1293265ed95a42861 🚀

---
+++ package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"org.minestombrick.movablestructures.api"}];updateSearchResults();
---
+++ type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org.minestombrick.movablestructures.api","l":"MovableStructure"}];updateSearchResults();
